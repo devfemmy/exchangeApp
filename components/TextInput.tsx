0e5c47e5fa8cefc34acc693265b10bb3ac2516be
devfemmy <odoyoakereleisrael@gmail.com>
2023-02-27 13:04:17 +0100

    update to test input

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable space-infix-ops */
 import React, {memo, ComponentProps} from 'react';
